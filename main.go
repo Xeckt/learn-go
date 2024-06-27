a10ddd9181653ef1db1cfc50ce48b6ea7ac42cf1
Xeckt <jordansavell1198@gmail.com>
2024-06-27 16:38:45 +0100

    edit: clear repo

--- main.go
+++ main.go
@@ -1,29 +1,4 @@
 package main
 
-import (
-	"github.com/bwmarrin/discordgo"
-	"log"
-	"os"
-	"os/signal"
-)
-
 func main() {
-	discord, err := discordgo.New("Bot " + "authentication token")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = discord.Open()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	sigch := make(chan os.Signal, 1)
-	signal.Notify(sigch, os.Interrupt)
-	<-sigch
-
-	err = discord.Close()
-	if err != nil {
-		log.Printf("could not close session: %s", err)
-	}
 }
